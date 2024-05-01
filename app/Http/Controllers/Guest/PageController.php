2711bba8d4b8bfad7c0e77ec19d89e1425800ec1
LeonardSuli <leonardsuli1991@gmail.com>
2024-05-01 11:26:57 +0200

    add filters

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -12,7 +12,33 @@ class PageController extends Controller
     public function index()
     {
 
-        $movies = Movie::all();
+        // // Tutti i film
+        // $movies = Movie::all();
+
+        // // Filtro con 2 parametri
+        // $movies = Movie::where('title', 'Forrest Gump')->get();
+
+        // // Filtro con 3 parametri
+        // $movies = Movie::where('vote', '>', '8.7')->get();
+
+        // // Filtro con orderBy
+        $movies = Movie::where('vote', '>', '8.6')
+            ->orderBy('title', 'desc')->get();
+
+        // // Filtro con limit
+        // $movies = Movie::where('vote', '>', '8.6')
+        //     ->orderBy('title', 'desc')
+        //     ->limit(3)->get();
+
+        // // Filtro con first
+        // $movie = Movie::where('vote', '>', '8.6')
+        //     ->first();
+        // dd($movie);
+
+        // // Filtro con find
+        // $movie = Movie::find(8); //il numero si riferisce all'id del film
+        // dd($movie);
+
 
         return view('home', compact('movies'));
     }
